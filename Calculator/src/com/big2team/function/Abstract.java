75fd9507ce883b7432d61f11d7d85735b8b20a66
SujinY8n <coray8n@gmail.com>
2021-09-08 14:58:23 +0900

    uploading.abstract

---
+++ Calculator/src/com/big2team/function/Abstract.java
@@ -0,0 +1,21 @@
+package com.big2team.function;
+
+public class Abstract {
+
+	//field 
+	
+	
+	//constructor
+	
+	public Abstract () {   //생성자는 class이름이랑 같으면 됨
+		
+	}
+	
+	//method
+	
+	public void abstractCal(int a, int b) {
+		System.out.println("뺼셈 :" + (a-b) );
+	}
+	
+	
+}//
